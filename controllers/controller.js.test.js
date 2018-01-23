f3fc41d1d978a41a7b8ce74eca8d1bd60db9df62
Steve Walker <captnwalker@gmail.com>
2018-01-23 01:39:53 -0500

    controller

--- controllers/controller.js.test.js
+++ controllers/controller.js.test.js
@@ -2,22 +2,22 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (exercise.js) to use its database functions.
-var exercise = require("../models/exercise.js");
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/cat.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  exercise.all(function(data) {
+  cat.all(function(data) {
     var hbsObject = {
-      exercises: data
+      cats: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/exercises", function(req, res) {
-  exercise.create([
+router.post("/api/cats", function(req, res) {
+  cat.create([
     "name", "sleepy"
   ], [
     req.body.name, req.body.sleepy
@@ -27,12 +27,12 @@ router.post("/api/exercises", function(req, res) {
   });
 });
 
-router.put("/api/exercises/:id", function(req, res) {
+router.put("/api/cats/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  exercise.update({
+  cat.update({
     sleepy: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -44,10 +44,10 @@ router.put("/api/exercises/:id", function(req, res) {
   });
 });
 
-router.delete("/api/exercises/:id", function(req, res) {
+router.delete("/api/cats/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  exercise.delete(condition, function(result) {
+  cat.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
